0b8e289b150470c3e485e7c1099c6e0902cfaadd
valentina.martinez11 <valentina.martinez11@gmail.com>
2021-03-06 10:32:07 -0300

    First adding + multiplication

---
+++ main.py
@@ -0,0 +1,14 @@
+hi = "Hello"
+print(hi[2])
+print("Hello"[2])
+if hi[2] == hi[3]:
+    print("this is true because we have double l")
+greeting = "hi " + "how " + "are " + "you " + "?"
+print(greeting)
+#multiplication
+print("bye"*2)
+if "hi" > "HI":
+    print("hi > HI")
+else:
+    print("hi < HI")
+print("1ello">"hello")
